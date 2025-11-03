94578080e170a01df0cf9cd458397396fbff7557
Mark O'Donnell <gold747@gmail.com>
2025-11-03 18:24:23 +1100

    Updated server to port 8080 and improved run.bat with network access info

--- run.bat
+++ run.bat
@@ -5,6 +5,7 @@ echo Make sure you have Python installed with required packages:
 echo pip install -r requirements.txt
 echo.
 echo Server will start on http://localhost:8080
+echo For local network access, use your IP address (e.g., http://192.168.1.126:8080)
 echo Press Ctrl+C to stop the server
 echo.
 echo IMPORTANT: Run this outside VS Code to avoid hangs!
